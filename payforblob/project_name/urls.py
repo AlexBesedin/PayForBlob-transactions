5aed1449c3482f03bfc80a9c34d81f91d93d40e9
AlexBeszedin <alebesedin@yandex.ru>
2023-04-01 10:59:23 +0300

    Правка стилей

--- payforblob/project_name/urls.py
+++ payforblob/project_name/urls.py
@@ -21,6 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('transactions.urls'))
-] 
-
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
