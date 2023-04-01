20d02e78fdba548153181b627e4647e38075daab
root <root@vm1140358.stark-industries.solutions>
2023-04-01 10:18:28 +0300

    Поправил стили, добавили место для лого типа

--- payforblob/project_name/settings.py
+++ payforblob/project_name/settings.py
@@ -117,11 +117,8 @@ USE_TZ = True
 
 
 
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -131,7 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
