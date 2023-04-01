bd630cdf7bec823d5229546d7131b0539d1ba8a7
AlexBeszedin <alebesedin@yandex.ru>
2023-04-02 01:13:50 +0300

    Доработки

--- payforblob/project_name/urls.py
+++ payforblob/project_name/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('transactions.urls')),
     path('admin/', admin.site.urls),
-    path('', include('transactions.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
