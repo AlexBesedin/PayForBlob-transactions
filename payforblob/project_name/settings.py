963f1ab6cfa92586c61667eaa889f9f789ff2ffa
AlexBeszedin <alebesedin@yandex.ru>
2023-04-25 15:48:32 +0300

    Добавлен Dockerfile, docker-compose

--- payforblob/project_name/settings.py
+++ payforblob/project_name/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
